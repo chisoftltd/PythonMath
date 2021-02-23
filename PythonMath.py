f7219e01d373e8b78a9cf61a1fc84a5982814a97
chisoftltd <chisoftltd@gmail.com>
2021-02-23 20:13:37 +0100

    first commit

---
+++ PythonMath.py
@@ -0,0 +1,34 @@
+# Python Math
+# Built-in Math Functions
+import os
+os.system("cls")
+
+a = min(5, 55, 105)
+b = max(105, 59, 9)
+
+print(a)
+print(b)
+print()
+
+c = abs(-7.90)
+print(c)
+print()
+
+d = pow(2, 3)
+print(d)
+print()
+
+# The Math Module
+import math
+
+e = math.sqrt(144)
+print(e)
+print()
+
+f = math.ceil(3.54)
+g = math.floor(3.54)
+print(f)
+print(g)
+print()
+
+print(math.pi)
